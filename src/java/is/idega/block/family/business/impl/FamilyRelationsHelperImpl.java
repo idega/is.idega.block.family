6831641ae2f00aecc92a36ed24683a8c830981eb
Valdas Žemaitis <valdas@idega.com>
2013-01-04 09:58:47 +0200

    Supporting legacy

--- src/java/is/idega/block/family/business/impl/FamilyRelationsHelperImpl.java
+++ src/java/is/idega/block/family/business/impl/FamilyRelationsHelperImpl.java
@@ -1,15 +1,15 @@
 package is.idega.block.family.business.impl;
 
+import is.idega.block.family.business.FamilyLogic;
+import is.idega.block.family.business.FamilyRelationsHelper;
+import is.idega.block.family.business.NoCohabitantFound;
+import is.idega.block.family.business.NoSpouseFound;
+
 import java.util.Collection;
 import java.util.logging.Level;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
-import is.idega.block.family.business.FamilyLogic;
-import is.idega.block.family.business.FamilyRelationsHelper;
-import is.idega.block.family.business.NoCohabitantFound;
-import is.idega.block.family.business.NoSpouseFound;
 
 import com.idega.core.business.DefaultSpringBean;
 import com.idega.core.contact.data.Email;
@@ -26,17 +26,17 @@ import com.idega.util.StringUtil;
 public class FamilyRelationsHelperImpl extends DefaultSpringBean implements FamilyRelationsHelper {
 
 	static final String BEAN_IDENTIFIER = "familyRelationsHelper";
-	
+
 	private User currentUser;
 	private User spouseOrCohabitant;
-	
+
 	private User getUser() {
 		if (currentUser == null) {
-			currentUser = getCurrentUser();
+			currentUser = getOldUser(getCurrentUser());
 		}
 		return currentUser;
 	}
-	
+
 	private User getSpouseOrCohabitant() {
 		if (spouseOrCohabitant == null) {
 			FamilyLogic familyLogic = getServiceInstance(FamilyLogic.class);
@@ -57,19 +57,20 @@ public class FamilyRelationsHelperImpl extends DefaultSpringBean implements Fami
 		}
 		return spouseOrCohabitant;
 	}
-	
+
+	@Override
 	@SuppressWarnings("unchecked")
 	public String getSpouseEmail() {
 		User spouse = getSpouseOrCohabitant();
 		if (spouse == null) {
 			return null;
 		}
-		
+
 		Collection<Email> emails = spouse.getEmails();
 		if (ListUtil.isEmpty(emails)) {
 			return null;
 		}
-		
+
 		String email = null;
 		EmailType mailType = null;
 		for (Email mail: emails) {
@@ -78,36 +79,40 @@ public class FamilyRelationsHelperImpl extends DefaultSpringBean implements Fami
 				email = mail.getEmailAddress();
 			}
 		}
-		
+
 		if (StringUtil.isEmpty(email)) {
 			email = emails.iterator().next().getEmailAddress();
 		}
-		
+
 		return email;
 	}
 
+	@Override
 	public String getSpouseName() {
 		User spouse = getSpouseOrCohabitant();
 		if (spouse == null) {
 			return null;
 		}
-		
+
 		return spouse.getName();
 	}
 
+	@Override
 	public String getSpousePersonalId() {
 		User spouse = getSpouseOrCohabitant();
 		if (spouse == null) {
 			return null;
 		}
-		
+
 		return spouse.getPersonalID();
 	}
-	
+
+	@Override
 	public String getSpouseMobile() {
 		return getPhoneByType(PhoneType.MOBILE_PHONE_ID);
 	}
 
+	@Override
 	public String getSpouseTelephone() {
 		return getPhoneByType(PhoneType.HOME_PHONE_ID);
 	}
@@ -118,14 +123,14 @@ public class FamilyRelationsHelperImpl extends DefaultSpringBean implements Fami
 		if (spouse == null) {
 			return null;
 		}
-		
+
 		Collection<Phone> phones = null;
 		try {
 			phones = spouse.getPhones(String.valueOf(phoneType));
 		} catch (Exception e) {
 			getLogger().log(Level.WARNING, "Error getting phones for " + spouse + " by type: " + phoneType);
 		}
-		
+
 		return ListUtil.isEmpty(phones) ? null : phones.iterator().next().getNumber();
 	}
 }
